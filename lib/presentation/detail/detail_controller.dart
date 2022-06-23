b582bf2d4015d38d71c914ba20ef65f8badec78e
Jeremia Christian Aditya Perdana <jejechristianap@gmail.com>
2022-06-23 10:48:51 +0700

    Update selected hero from similiar heroes and handle backPress

--- lib/presentation/detail/detail_controller.dart
+++ lib/presentation/detail/detail_controller.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_dota_hero_list/common/styles/app_colors.dart';
 import 'package:flutter_dota_hero_list/data/model/hero_stats.dart';
@@ -12,15 +14,19 @@ class DetailController extends GetxController {
   var heroStat = HeroStats();
   late List<HeroStats> heroesStat;
   List<HeroStats> similiarHeroes = [];
+  List<HeroStats> selectedHeroes = [];
+  var backStackPosition = 0.obs;
 
   @override
   void onInit() {
     super.onInit();
+    log('selectedHeroes onInit: ${selectedHeroes.length}');
     final selectedHero = Get.parameters['selectedHero'];
     final heroes = Get.parameters['heroes'];
     if (selectedHero != null && heroes != null) {
       heroStat = heroStatFromJson(selectedHero);
       heroesStat = heroStatsFromJson(heroes);
+      log('${selectedHeroes.length}');
       getSimiliarHeroes();
     } else {
       Get.snackbar(
@@ -43,5 +49,26 @@ class DetailController extends GetxController {
     similiarHeroes = _getSimiliarHeroes.call(param);
     similiarHeroes.reversed;
     update();
+  }
+
+  void updateSelectedHeroes(
+      {required HeroStats hero, bool isBackPress = false}) {
+    if (!isBackPress) {
+      selectedHeroes.add(heroStat);
+      backStackPosition.value++;
+    }
+    heroStat = hero;
+    log('selectedHeroes: ${selectedHeroes.length}\nbackStackPosition: $backStackPosition');
+    getSimiliarHeroes();
+  }
+
+  Future<bool> handleBackPress() async {
+    if (backStackPosition > 0) {
+      backStackPosition.value--;
+      updateSelectedHeroes(
+          hero: selectedHeroes[backStackPosition.value], isBackPress: true);
+      return false;
+    }
+    return true;
   }
 }
