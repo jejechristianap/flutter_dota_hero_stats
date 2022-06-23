e086982b4855eeb73913ee9c1ea54b1a0c461ffd
Jeremia Christian Aditya Perdana <jejechristianap@gmail.com>
2022-06-23 14:21:55 +0700

    Added few test cases

--- test/domain/usecase/usecase_test.dart
+++ test/domain/usecase/usecase_test.dart
@@ -6,12 +6,13 @@ import '../../test_helper.dart';
 
 void main() {
   group('Role Filtered', () {
-    test('Filtered Hero {onSelected = true, index = 1} should contain 3 Heroes',
+    test(
+        'Filtered Hero {onSelected = true, index = 1, role = [Carry, Support]} should contain 4 Heroes',
         () {
       var index = 1;
       var onSelected = true;
 
-      List<dynamic> roleFiltered = [];
+      List<dynamic> roleFiltered = ['Carry', 'Support'];
 
       final param = {
         'index': index,
@@ -25,11 +26,34 @@ void main() {
       final filtered = useCase.call(param);
       List<HeroStats> result = filtered['heroesFiltered'];
 
-      expect(result.length, 3);
+      expect(result.length, 4);
     });
 
     test(
-        'Filtered Hero with {onSelected = false, index = 1} should return 0 Hero',
+        'Filtered Hero with {onSelected = false, index = 1, role = [All]} should return 5 Heroes',
+        () {
+      var index = 1;
+      var onSelected = false;
+
+      List<dynamic> roleFiltered = ['All'];
+
+      final param = {
+        'index': index,
+        'onSelected': onSelected,
+        'roles': TestHelper.roles,
+        'roleFiltered': roleFiltered,
+        'heroes': TestHelper.heroes
+      };
+
+      final useCase = FilterRoles();
+      final filtered = useCase.call(param);
+      List<HeroStats> result = filtered['heroesFiltered'];
+
+      expect(result.length, 5);
+    });
+
+    test(
+        'Filtered Hero with {onSelected = false, index = 1, roles = []} should return 0 Hero',
         () {
       var index = 1;
       var onSelected = false;
@@ -49,6 +73,29 @@ void main() {
       List<HeroStats> result = filtered['heroesFiltered'];
 
       expect(result.length, 0);
+    });
+
+    test(
+        'Filtered Hero with {onSelected = false, index = 1} localizeName should return Hero1',
+        () {
+      var index = 1;
+      var onSelected = true;
+
+      List<dynamic> roleFiltered = ['Carry'];
+
+      final param = {
+        'index': index,
+        'onSelected': onSelected,
+        'roles': TestHelper.roles,
+        'roleFiltered': roleFiltered,
+        'heroes': TestHelper.heroes
+      };
+
+      final useCase = FilterRoles();
+      final filtered = useCase.call(param);
+      List<HeroStats> result = filtered['heroesFiltered'];
+
+      expect(result[0].localizedName, 'Hero1');
     });
   });
 }
